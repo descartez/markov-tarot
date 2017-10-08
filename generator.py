a6376659bb684bab43c5861a51aca096fec003d0
Ryan Stong <rvanstong@gmail.com>
2017-10-08 14:09:00 -0700

    finishes class creation for characters; next steps to implement multiple models for text creation

--- generator.py
+++ generator.py
@@ -1,11 +1,36 @@
 import markovify
 
-with open("hp_lovecraft_mountains_of_madness.txt") as f:
-    text = f.read()
+class Character:
+    def __init__(self):
+        self.name = ""
+        self.bio = ""
 
-# Build the model.
-text_model = markovify.Text(text)
+    def create_bio(self, name, bio):
+        self.name = name
+        self.bio = bio
 
-# Print three randomly-generated sentences of no more than 140 characters
-for i in range(3):
-    print(text_model.make_short_sentence(140))
+    def create_model(self, file):
+        with open(file) as f:
+            text = f.read()
+        self.model = markovify.Text(text)
+
+    def speak_bio(self):
+        print("-"*15)
+        print(self.name)
+        print(self.bio)
+        print("-"*15)
+
+    def speak_tweet(self):
+        print(self.model.make_short_sentence(140))
+
+lovecraft = Character()
+lovecraft.create_bio("H.P. Lovecraft", "A xenophobic creepy writer.")
+lovecraft.create_model("hp_lovecraft_mountains_of_madness.txt")
+lovecraft.speak_bio()
+lovecraft.speak_tweet()
+
+gaiman = Character()
+gaiman.create_bio("Neil Gaiman", "An excellent, empathetic man and writer.")
+gaiman.create_model("neil_gaiman_click_clack.txt")
+gaiman.speak_bio()
+gaiman.speak_tweet()
